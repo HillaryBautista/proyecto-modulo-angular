b8a20272f037c883b3e6553110e051efa1d18152
Hillary Bautista <hillarybautista2301@gmail.com>
2024-09-23 11:36:42 -0500

    Environment prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   version: '1.0.0',
   apiPost: 'https://jsonplaceholder.typicode.com/',
-  apiImg: 'https://picsum.photos/v2/list',
+  apiImg: 'https://picsum.photos/v2/',
 };
